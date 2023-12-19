34b9a7f0bfcc2e8d532d29ddc4fe28081b52db63
phoopwint <phoopwint445@gmail.com>
2023-12-19 11:53:14 +0630

    test: themeSwicher

--- src/components/ThemeSwicher.jsx
+++ src/components/ThemeSwicher.jsx
@@ -7,7 +7,7 @@ const ThemeSwicher = () => {
   const [ theme,setTheme ] = useState(null);
 
   useEffect(() => {
-    const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    const prefersDarkMode = window.matchMedia('(prefers-color-scheme: light)').matches;
     setTheme(prefersDarkMode ? 'dark' : 'light');    
   },[])
 
@@ -26,7 +26,7 @@ const ThemeSwicher = () => {
 
   return (
     <div className='md:ml-7'>
-        <input type="checkbox" checked={checked} onChange={handleToggle} id='switcher' className='hidden'/>
+        <input type="checkbox" checked={checked} onChange={handleToggle} id='switcher'/>
         <label htmlFor='switcher' className='relative flex items-center w-15 h-8 rounded-full px-2 gap-1 transition bg-amber-200 cursor-pointer z-10'>
             <div className='w-5 h-5 flex items-center justify-center'>
                 <FiSun className='text-white'/>
@@ -34,7 +34,7 @@ const ThemeSwicher = () => {
             <div className='w-5 h-5 flex items-center justify-center'>
                 <FiMoon className='text-white'/>
             </div>  
-            <div className={`absolute w-5 h-5 rounded-full bg-amber-600 -z-10 transition-all duration-700 ${checked ? 'left-[8px]' : 'left-[32px]'}`} />        
+            <div className={`absolute w-5 h-5 rounded-full bg-amber-600 -z-10 transition-all duration-700 ${checked ? 'left-[32px]' : 'left-[8px]'}`} />        
         </label>
     </div>
   )
